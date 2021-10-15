9093e4fc5ddebffdc6da286a61d941bd1ba52eba
Noah Schmidt <noschmidt@stud.hs-heilbronn.de>
2021-10-15 23:43:54 +0200

    Updated TemporaryWorker

--- src/main/java/gse/employee/TemporaryWorker.java
+++ src/main/java/gse/employee/TemporaryWorker.java
@@ -1,4 +1,56 @@
 package gse.employee;
 
-public class TemporaryWorker {
+/**
+ * This class represents a "Zeitarbeiter".
+ *
+ * @author Noah Schmidt
+ */
+public class TemporaryWorker extends Employee {
+
+  private final float monthlySalary;
+  private int hoursWorked;
+
+  public TemporaryWorker(String forename, String surname, float monthlySalary) {
+    super(forename, surname);
+    this.monthlySalary = monthlySalary;
+    this.hoursWorked = 0;
+  }
+
+  /**
+   * Return the fixed monthly salary.
+   *
+   * @return Salary determined by the employment contract.
+   */
+  public float getMonthlySalary() {
+    return monthlySalary;
+  }
+
+  /**
+   * Returns the hours worked this month.
+   *
+   * @return Hours worked this month.
+   */
+  public int getHoursWorked() {
+    return hoursWorked;
+  }
+
+  /**
+   * Sets the hours worked this month.
+   *
+   * @param hoursWorked The hours worked.
+   */
+  public void setHoursWorked(int hoursWorked) {
+    this.hoursWorked = hoursWorked;
+  }
+
+
+  /**
+   * Calculates the salary for this month.
+   *
+   * @return Salary for the current month.
+   */
+  @Override
+  public float calculateSalary() {
+    return monthlySalary * hoursWorked;
+  }
 }
