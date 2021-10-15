dedb72e76c1e1e80b99c5489341e6647d180ca97
Marvin Simon <simon.marvin.1@web.de>
2021-10-15 17:29:27 +0200

    Project structure

---
+++ src/main/java/gse/mitarbeiter/Mitarbeiter.java
@@ -0,0 +1,4 @@
+package gse.mitarbeiter;
+
+public class Mitarbeiter {
+}
---
+++ src/test/java/gse/mitarbeiter/TestMitarbeiter.java
@@ -0,0 +1,17 @@
+package gse.mitarbeiter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+    class TestMitarbeiter {
+
+    @Test
+    public void GeneralFailTest() {
+        Assert.assertNotNull(null);
+    }
+}
