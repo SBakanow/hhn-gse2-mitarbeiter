ea2d71c4f6454443998de94ff7a98251d499ba4b
Dennis Schneider <dschneider@stud.hs-heilbronn.de>
2021-10-16 14:06:23 +0200

    First version of NonTarifEmployee

--- src/main/java/gse/employee/NonTariffEmployee.java
+++ src/main/java/gse/employee/NonTariffEmployee.java
@@ -1,4 +1,35 @@
 package gse.employee;
 
-public class NonTariffEmployee {
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Extends Employee class
+ *
+ * @author Dennis Schneider
+ */
+
+public class NonTariffEmployee extends Employee {
+
+  private final float monthlySalary;
+  private Date date;
+
+
+  public NonTariffEmployee(String forename, String surname, float monthlySalary) {
+    super(forename, surname);
+    this.monthlySalary = monthlySalary;
+    Date date = new Date();
+
+  }
+
+  public float getMonthlySalary() {
+    return monthlySalary;
+  }
+//TODO
+  @Override
+  public float calculateSalary() {
+    yearlySalaryToThisDate = monthlySalary * (date.getMonth() + 1);
+    return monthlySalary;
+  }
 }
