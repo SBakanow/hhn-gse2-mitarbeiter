885c0ef5551e7c9a17a6415cf97ba9091850b7ea
Sergej Bakanow <s.bakanow@gmail.com>
2021-10-20 12:41:54 +0200

    Added first draft for second excercise

--- src/main/java/gse/employee/SalariedEmployee.java
+++ src/main/java/gse/employee/SalariedEmployee.java
@@ -5,25 +5,46 @@ package gse.employee;
  *
  * @author Sergej Bakanow
  */
-public class SalariedEmployee extends Employee {
+public class SalariedEmployee implements Employee {
 
   private float monthlySalary;
   private float overtimeRate;
   private int hoursWorkedOvertime;
+  private final int WEEKLYWORKINGTIME = 40;
 
-  private int weeklyWorkingTime;
+  private String surname;
+  private String forename;
+  private float yearlySalaryToThisDate;
+  private float hourlySalary;
 
-  public SalariedEmployee(String forename, String surname, float monthlySalary, float overtimeRate,
-                          int weeklyWorkingTime) {
-    super(forename, surname);
-    if (monthlySalary >= 0 && overtimeRate >= 0 && overtimeRate <= 1
-        && weeklyWorkingTime >= 1 && weeklyWorkingTime <= 48) {
+  private int monthCounter;
+
+  /**
+   * constructor of the SalariedEmployee class.
+   *
+   * @param forename      the first name of the worker.
+   * @param surname       the surname of the worker.
+   * @param monthlySalary the monthly Salary the worker receives.
+   * @param overtimeRate  the overtime rate in percent.
+   */
+  public SalariedEmployee(String forename, String surname, float monthlySalary,
+                          float overtimeRate) {
+    if (forename != null && !forename.isBlank()) {
+      this.forename = forename;
+    } else {
+      this.forename = "Jane";
+    }
+    if (surname != null && !surname.isBlank()) {
+      this.surname = surname;
+    } else {
+      this.surname = "Doe";
+    }
+    calculateHourlySalary(monthlySalary);
+    if (hourlySalary >= 9.60 && overtimeRate >= 0 && overtimeRate <= 1) {
       this.monthlySalary = monthlySalary;
       this.overtimeRate = overtimeRate;
-      this.weeklyWorkingTime = weeklyWorkingTime;
-    }
-    else {
-      throw new NullPointerException("Some values were not in accordance with the law.");
+    } else {
+      throw new IllegalArgumentException("Some values were not in accordance with the law.");
     }
   }
 
@@ -59,8 +80,8 @@ public class SalariedEmployee extends Employee {
    *
    * @return Returns the weekly working time.
    */
-  public int getWeeklyWorkingTime() {
-    return weeklyWorkingTime;
+  public int getWEEKLYWORKINGTIME() {
+    return WEEKLYWORKINGTIME;
   }
 
   /**
@@ -79,11 +100,83 @@ public class SalariedEmployee extends Employee {
    */
   @Override
   public float calculateSalary() {
-    float hourlySalary = (monthlySalary / (weeklyWorkingTime * 4.33f));
+    float hourlySalary = (monthlySalary / (WEEKLYWORKINGTIME * 4.33f));
     float salary =
         monthlySalary + ((hourlySalary + (hourlySalary * overtimeRate)) * hoursWorkedOvertime);
     salary = calculateMonth(salary);
     hoursWorkedOvertime = 0;
     return salary;
+  }
+
+  /**
+   * Returns the hourly salary of the worker.
+   *
+   * @return the hourly salary of the worker.
+   */
+  public float getHourlySalary() {
+    return hourlySalary;
+  }
+
+  /**
+   * Calculates the hourly salary with the given monthly salary.
+   *
+   * @param monthlySalary the monthly salary of the worker.
+   */
+  public void calculateHourlySalary(float monthlySalary) {
+    hourlySalary = (monthlySalary / (WEEKLYWORKINGTIME * 4f));
+  }
+
+  /**
+   * Getter for the forename.
+   *
+   * @return Forename of the employee
+   */
+  public String getForename() {
+    return forename;
+  }
+
+  /**
+   * Get the Month Counter.
+   *
+   * @return Month Counter
+   */
+  public int getMonthCounter() {
+    return monthCounter;
+  }
+
+  /**
+   * Getter SummedUpSalary.
+   *
+   * @return Summed up salary to this day
+   */
+  public float getYearlySalaryToThisDate() {
+    return yearlySalaryToThisDate;
+  }
+
+  /**
+   * Getter for the surname.
+   *
+   * @return Surname of the employee
+   */
+  public String getSurname() {
+    return surname;
+  }
+
+  /**
+   * Method to reset the yearly salary after a year has passed.
+   * Implemented as protected as no other person is allowed to call it.
+   *
+   * @param monthlySalary Monthly Salary that gets added.
+   */
+  public float calculateMonth(float monthlySalary) {
+    float result = (Math.round(monthlySalary * 100.0f) / 100.0f);
+    if (monthCounter < 12) {
+      yearlySalaryToThisDate += result;
+      monthCounter++;
+    } else {
+      yearlySalaryToThisDate = result;
+      monthCounter = 0;
+    }
+    return result;
   }
 }
